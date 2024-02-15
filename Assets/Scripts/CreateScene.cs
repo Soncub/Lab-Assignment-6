bea73333f886bbc27b815c7bff1b56a0c52fa3e0
Andrew Mokhtary <akouroshm@gmail.com>
2024-02-15 13:58:27 -0500

    Script

---
+++ Assets/Scripts/CreateScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateScene : MonoBehaviour
+{
+    public int sizeOfForest;
+    public int stonesRequired;
+    public GameObject[] trees;
+    public GameObject[] stones;
+    void Start()
+    {
+        InitializeVariables();
+        CreateGround();
+        CreateRandomForest();
+        CreatePyramid();
+    }
+    void InitializeVariables()
+
+    void CreateGround()
+
+    void CreateRandomForest()
+
+    void CreatePyramid()
+}
